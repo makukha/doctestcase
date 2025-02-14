de357fb8db407943c538781220d56a7841aa4121
Michael Makukha <m.makukha@gmail.com>
2025-02-14 20:47:03 +0200

    Fix docs

--- tests/usage/param-base.py
+++ tests/usage/param-base.py
@@ -2,15 +2,16 @@ from doctest import ELLIPSIS
 import os.path
 import shutil
 import tempfile
-from unittest import TestCase, skip
+from unittest import TestCase
 
 from doctestcase import doctestcase
 
 
-@skip('base class')
 @doctestcase(options=ELLIPSIS, cwd='.')
 class ChdirTestCase(TestCase):
     def setUp(self):
+        if self.__class__ is ChdirTestCase:
+            self.skipTest('base class')  # no tests of the base class itself
         self.temp = tempfile.mkdtemp()
         self.prev = os.getcwd()
         cwd = os.path.join(self.temp, self.__doctestcase__.kwargs['cwd'])
